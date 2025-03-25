07b1ed4ffb80d75f9a033fe85c9bc3c461f5faa3
Víctor Alfonso Orellano Calambás <74632476+Darkv1c@users.noreply.github.com>
2025-03-24 19:17:06 -0500

    implement go router

--- lib/presentations/screens/home_screen.dart
+++ lib/presentations/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_item.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -38,7 +39,7 @@ class _MenuListTile extends StatelessWidget {
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subtitle),
       onTap: () {
-        Navigator.pushNamed(context, menuItem.route);
+        context.push(menuItem.route);
       },
     );
   }
---
+++ lib/presentations/screens/screens.dart
@@ -0,0 +1,3 @@
+export 'buttons_screen.dart';
+export 'cards_screen.dart';
+export 'home_screen.dart';
