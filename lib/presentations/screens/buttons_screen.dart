8b86dcf010dcb48c618505a04701742106f52165
Víctor Alfonso Orellano Calambás <74632476+Darkv1c@users.noreply.github.com>
2025-03-24 19:53:08 -0500

    add basic buttons to buttons page

--- lib/presentations/screens/buttons_screen.dart
+++ lib/presentations/screens/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
   static const String name = 'buttons_screen';
@@ -9,7 +10,81 @@ class ButtonsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Buttons Screen')),
-      body: Placeholder(),
+      body: _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_back),
+        onPressed: () {
+          context.pop();
+        },
+      ),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: SizedBox.expand(
+        child: Wrap(
+          spacing: 8,
+          runSpacing: 8,
+          alignment: WrapAlignment.center,
+          children: [
+            /** Elevated */
+            ElevatedButton(onPressed: () {}, child: const Text('Elevated')),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Elevated disabled'),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              label: const Text('Elevated with Icon'),
+              icon: const Icon(Icons.add),
+            ),
+            /** Filled */
+            FilledButton(onPressed: () {}, child: const Text('Filled')),
+            const FilledButton(
+              onPressed: null,
+              child: Text('Elevated disabled'),
+            ),
+            FilledButton.icon(
+              onPressed: () {},
+              label: const Text('Elevated with Icon'),
+              icon: const Icon(Icons.add),
+            ),
+            /** Outlined */
+            OutlinedButton(onPressed: () {}, child: const Text('Filled')),
+            const OutlinedButton(
+              onPressed: null,
+              child: Text('Elevated disabled'),
+            ),
+            OutlinedButton.icon(
+              onPressed: () {},
+              label: const Text('Elevated with Icon'),
+              icon: const Icon(Icons.add),
+            ),
+            /** TextButton */
+            TextButton(onPressed: () {}, child: const Text('Filled')),
+            const TextButton(onPressed: null, child: Text('Elevated disabled')),
+            TextButton.icon(
+              onPressed: () {},
+              label: const Text('Elevated with Icon'),
+              icon: const Icon(Icons.add),
+            ),
+            /** IconButton */
+            IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+            const IconButton(onPressed: null, icon: Icon(Icons.add)),
+            IconButton.filled(onPressed: () {}, icon: const Icon(Icons.add)),
+            IconButton.outlined(onPressed: () {}, icon: const Icon(Icons.add)),
+            IconButton.filledTonal(
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
