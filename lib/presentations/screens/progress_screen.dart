0defe91590d85d57a8a35f081e7cc04f5e519abc
Víctor Alfonso Orellano Calambás <74632476+Darkv1c@users.noreply.github.com>
2025-03-26 14:52:17 -0500

    progress indicators stream Builder

--- lib/presentations/screens/progress_screen.dart
+++ lib/presentations/screens/progress_screen.dart
@@ -1,5 +1,48 @@
 import 'package:flutter/material.dart';
 
+class _IndicatorData {
+  final String name;
+  final Widget indicator;
+
+  const _IndicatorData(this.name, this.indicator);
+}
+
+List<_IndicatorData> _getIndicators({double? value}) => [
+  _IndicatorData(
+    'Circular',
+    CircularProgressIndicator(strokeWidth: 2, value: value),
+  ),
+  _IndicatorData(
+    'Linear',
+    SizedBox(width: 100, child: LinearProgressIndicator(value: value)),
+  ),
+];
+
+_wrapIndicator(String name, Widget child) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    spacing: 20,
+    children: [Text(name), child, SizedBox()],
+  );
+}
+
+_wrapIndicatorGroup(String name, List<Widget> children) {
+  return Column(
+    spacing: 20,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: [
+      Text(name),
+      IntrinsicHeight(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          spacing: 20,
+          children: children,
+        ),
+      ),
+    ],
+  );
+}
+
 class ProgressScreen extends StatelessWidget {
   static String name = 'Progress Screen';
 
@@ -14,47 +57,8 @@ class ProgressScreen extends StatelessWidget {
   }
 }
 
-class _IndicatorData {
-  final String name;
-  final Widget indicator;
-
-  const _IndicatorData(this.name, this.indicator);
-}
-
 class _ProgressIndicatorsView extends StatelessWidget {
-  final List<_IndicatorData> indicators = const [
-    _IndicatorData('Circular', CircularProgressIndicator(strokeWidth: 2)),
-    _IndicatorData(
-      'Linear',
-      SizedBox(width: 100, child: LinearProgressIndicator()),
-    ),
-  ];
-
-  wrapIndicator(String name, Widget child) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      spacing: 20,
-      children: [Text(name), child, SizedBox()],
-    );
-  }
-
-  wrapIndicatorGroup(String name, List<Widget> children) {
-    return Column(
-      spacing: 20,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text(name),
-        IntrinsicHeight(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            spacing: 20,
-            children: children,
-          ),
-        ),
-      ],
-    );
-  }
-
+  final _indicators = _getIndicators();
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -62,23 +66,33 @@ class _ProgressIndicatorsView extends StatelessWidget {
       child: Column(
         spacing: 40,
         children: [
-          wrapIndicatorGroup('Uncontrolled', [
-            for (final indicator in indicators)
-              wrapIndicator(indicator.name, indicator.indicator),
+          _wrapIndicatorGroup('Uncontrolled', [
+            for (final indicator in _indicators)
+              _wrapIndicator(indicator.name, indicator.indicator),
           ]),
-          wrapIndicatorGroup('Controlled', [
-            for (final indicator in indicators)
-              wrapIndicator(indicator.name, indicator.indicator),
-          ]),
+          _ControlledIndicators(),
         ],
       ),
     );
   }
 }
 
-class _ControlledIndicator extends StatelessWidget {
+class _ControlledIndicators extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CircularProgressIndicator();
+    return StreamBuilder(
+      stream: Stream.periodic(
+        const Duration(milliseconds: 100),
+        (count) => count / 100,
+      ).takeWhile((value) => value < 100),
+      builder: (context, snapshot) {
+        final indicators = _getIndicators(value: snapshot.data ?? 0);
+
+        return _wrapIndicatorGroup('Controlled', [
+          for (final indicator in indicators)
+            _wrapIndicator(indicator.name, indicator.indicator),
+        ]);
+      },
+    );
   }
 }
